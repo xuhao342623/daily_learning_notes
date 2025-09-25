6de24f52092a09584e3c1beafe601b950aa6e538
xuhao342623 <1265149692@qq.com>
2025-09-25 20:53:09 +0800

    0925_1

---
+++ cpp/1.cpp
@@ -0,0 +1,23 @@
+ #include <iostream>
+ using namespace std;
+
+
+ int main()
+ {
+    //1、整形
+    // int a = 0;
+    // cout << "请给整形变量a赋值: " << endl;
+    // cin >> a;
+    // cout << "整型变量 a = " << a << endl;
+    //2、浮点型
+
+    //3、字符型
+
+    //4、字符串型
+
+    //5、布尔类型
+
+    system("pause");
+
+    return 0;
+ }
